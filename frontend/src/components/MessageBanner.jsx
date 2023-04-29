cfc46bea934f58c5486a52b5f0e8e7419d084ab4
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-04-28 23:06:10 -0400

    add zindex to banner

--- frontend/src/components/MessageBanner.jsx
+++ frontend/src/components/MessageBanner.jsx
@@ -3,7 +3,7 @@ import { Alert, AlertDescription, AlertIcon } from "@chakra-ui/react";
 
 const MessageBanner = ({ message, type = "error" }) => {
   return (
-    <Alert status={type} variant="left-accent">
+    <Alert status={type} variant="left-accent" zIndex={999}>
       <AlertIcon />
       <AlertDescription>{message}</AlertDescription>
     </Alert>
